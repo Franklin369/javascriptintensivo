ec2389faee41aa73689a65b047ddefa880b45239
Franklin369 <codigo369.com@gmail.com>
2022-10-20 13:56:54 -0500

    Main Project 17

---
+++ 17 Rodolfo-swiper/script.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper(".swiper-container",{
+  effect:"coverflow",
+  loop:true,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect:{
+    rotate: 20,
+    stretch: 0,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true
+  },
+  pagination:{
+    el:".swiper-pagination"
+  }
+
+})
